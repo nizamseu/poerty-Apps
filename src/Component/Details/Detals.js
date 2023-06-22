834a40320af697811e62c29504da5ec366cbc606
nizamseu <nizamcse.seu@gmail.com>
2023-06-22 17:05:23 +0600

    Detail Page

--- src/Component/Details/Detals.js
+++ src/Component/Details/Detals.js
@@ -1,9 +1,10 @@
 import { View, Text } from "react-native";
 import PoetryBanner from "../Share/PoetryBanner";
+import { ListItem } from "react-native-elements";
 export default function Details() {
   return (
     <View style={{ flex: 1, alignItems: "center", justifyContent: "center" }}>
-      <PoetryBanner />
+      <Text>Hello</Text>
     </View>
   );
 }
--- src/Component/DetailsScreen/DetailsScreen.js
+++ src/Component/DetailsScreen/DetailsScreen.js
@@ -1,10 +1,21 @@
 import { View, Text } from "react-native";
 import PoetryBanner from "../Share/PoetryBanner";
+import { ListItem } from "react-native-elements";
 export default function DetailsScreen() {
   return (
     <View style={{ flex: 1, alignItems: "center", justifyContent: "center" }}>
       <Text>Details Screen</Text>
-      <PoetryBanner />
+      <ListItem
+        leftAvatar={{
+          title: "Nizam Uddin",
+          source: { uri: avatar_url },
+          showAccessory: true,
+        }}
+        title="Babu"
+        subtitle={role}
+        chevron
+      />
+      ;
     </View>
   );
 }
